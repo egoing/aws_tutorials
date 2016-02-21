9baffb581794885ca396491bc3dacfbbbfb8d577
egoing <egoing@gmail.com>
2016-02-21 16:38:22 +0000

    add s3 usage

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 var AWS = require('aws-sdk');
+var formidable = require('formidable');
+var fs = require('fs');
 AWS.config.region = 'ap-northeast-2';
 var ec2  = new AWS.EC2();
 app.get('/', function(req, res){
 	res.send('Hello world');
 });
-app.get('/upload', function(req, res){
+app.get('/s3', function(req, res){
 	var output = `
 <html>
 <body>
@@ -19,13 +21,29 @@ app.get('/upload', function(req, res){
 	`;
 	res.send(output);
 })
-/*
+app.post('/upload_receiver', function(req, res){
+        var form = new formidable.IncomingForm()
+        form.parse(req, function(err, fields, files){
+		var s3 = new AWS.S3();
+		var params = {
+		  Bucket: 'codingeverybody2', /* required */
+		  Key: files.userfile.name, /* required */
+		  ACL: 'public-read',
+		  Body: fs.readFileSync(files.userfile.path)
+		};
+		console.log(params);
+		s3.putObject(params, function(err, data) {
+		  	if (err) console.log(err, err.stack); // an error occurred
+		  	else     console.log(data);           // successful response
+			res.json([err, fields, files, data]);
+		});
+        });
+});
 app.get('/ec2', function(req, res){
 	ec2.describeInstances({}, function(err, data) {
 		res.json(data);
 	});
 });
-*/
 app.listen(80, function(){
 	console.log('Connect 80 port');
 });
