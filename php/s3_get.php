e9659cddb7ffbba95b6195841354acb998d25c91
Ubuntu <ubuntu@ip-172-31-17-162.ap-northeast-2.compute.internal>
2016-02-22 23:41:13 +0000

    Array의 형식 변경

--- php/s3_get.php
+++ php/s3_get.php
@@ -2,9 +2,9 @@
 require 'vendor/autoload.php';
 $param = Array('region'=>'ap-northeast-2', 'version'=>'2006-03-01');
 $s3 = new Aws\S3\S3Client($param);
-$s3->getObject([
+$s3->getObject(Array(
     'Bucket'=>'codingeverybody2',
     'Key'=>'sample.txt',
     'SaveAs'=>fopen('sample_saved.txt', 'w')
-]);
+));
 ?>
